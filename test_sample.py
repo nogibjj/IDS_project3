23129215ed91087596c98f3ad5883c281b668618
Ahmed Ibrahim <58277837+ibrahimjuhas@users.noreply.github.com>
2022-12-06 22:36:50 +0000

    adding sample files

---
+++ test_sample.py
@@ -0,0 +1,35 @@
+import sqlite3
+import os
+from faker import Faker
+
+connection = sqlite3.connect('test_sample.db')
+
+
+table = 'CREATE TABLE people (id integer primary key, name TEXT, surname TEXT)'
+
+cursor = connection.cursor()
+
+#cursor.execute(table)
+
+connection.commit()
+#generating random fake names
+
+fake = Faker()
+names = [fake.name().split() for i in range(100)]
+names = [name for name in names if len(name) == 2]
+
+connection = sqlite3.connect('test_sample.db')
+
+cursor = connection.cursor()
+
+insert_query = 'INSERT INTO people(name,surname) VALUES (?,?)'
+
+for name in names:
+    cursor.execute(insert_query, name)
+
+    connection.commit()
+
+
+select_query = 'SELECT * from people LIMIT 6'
+for i in cursor.execute(select_query):
+    print(i)
